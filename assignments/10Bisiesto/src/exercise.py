6e0e7ebc8a4e38db20cb700352f14b98d67bb58f
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-30 21:15:35 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,15 @@
-
+def es_bisiesto(año):
+    if año % 4 == 0:
+        return True
+    else:
+        return False
+    
 def main():
     #escribe tu código abajo de esta línea
+    
+    año = int(input(""))
+    print (es_bisiesto(año))
+    
     pass
 
 if __name__=='__main__':
