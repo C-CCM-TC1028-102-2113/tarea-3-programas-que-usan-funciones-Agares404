2dc7fda14d09976cf8e1c7b352c1975309bfb427
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-30 20:44:12 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,18 @@
+def volumen(b,h,a):
+    vol = b*h*a
+    return vol
 
 def main():
     #escribe tu código abajo de esta línea
+    
+    base = float(input("dame la base: "))
+    alt_base = float(input("dame la altura de la base: "))
+    alt = float(input("dame la altura del prisma: "))
+
+    final = volumen(base, alt_base, alt)
+
+    print("El voumen del prisma es ", final)
+    
     pass
 
 if __name__=='__main__':
