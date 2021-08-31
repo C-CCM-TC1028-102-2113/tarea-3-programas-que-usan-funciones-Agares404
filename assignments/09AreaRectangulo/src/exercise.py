b42064ec3acb0466f5ea5fbf245694ddc8d29f90
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-30 21:02:48 -0500

    Update exercise.py

--- assignments/09AreaRectangulo/src/exercise.py
+++ assignments/09AreaRectangulo/src/exercise.py
@@ -1,6 +1,17 @@
+def area(b,h):
+    a = b*h
+    return a
 
 def main():
     #escribe tu código abajo de esta línea
+    
+    base = float(input("dame la base del rectangulo: "))
+    alt = float(input("dame la altura del rectangulo: "))
+
+    res = area(base, alt)
+
+    print("El voumen del prisma es ", res)
+
     pass
 
 if __name__=='__main__':
